45b3f72ffa2f35a528fff20261d99290e3d442f0
afrinaldi-knitto <it17.knitto@gmail.com>
2024-12-30 16:58:24 +0700

    ADD : assets

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,113 +1,171 @@
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
+  ActivityIndicator,
   View,
+  StyleSheet,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import Config from 'react-native-config';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import DeviceInfo from 'react-native-device-info';
 
 const Login = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  console.log('INI CONFIGNYA CUY ', Config.URL);
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [isRefreshing, setIsRefreshing] = useState<boolean>(false);
+  const currentVersion = DeviceInfo.getVersion();
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <View style={styles.container}>
+      <Image
+        style={styles.bgImage}
+        source={require('../../assets/dummy/bg.jpg')}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>{Config.URL}</Text> to change
-            this screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <Image
+        style={{width: 100, marginTop: 50, marginBottom: 20, height: 100}}
+        source={require('../../assets/logo/logo.png')}
+      />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputs}
+          placeholder="Username"
+          underlineColorAndroid="transparent"
+          value={username}
+          onChangeText={currentUsername => setUsername(currentUsername)}
+          autoFocus={true}
+          returnKeyType="next"
+          onSubmitEditing={() => {}}
+        />
+        <Image
+          style={styles.inputIcon}
+          source={require('../../assets/icon/email.png')}
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputs}
+          placeholder="Password"
+          secureTextEntry={true}
+          underlineColorAndroid="transparent"
+          value={password}
+          onChangeText={currentPassword => setPassword(currentPassword)}
+          onSubmitEditing={() => {}}
+        />
+        <Image
+          style={styles.inputIcon}
+          source={require('../../assets/icon/key.png')}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.btnForgotPassword} onPress={() => {}}>
+        <Text style={styles.btnText}>Forgot Password?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.buttonContainer, styles.loginButton]}
+        onPress={() => {}}
+        disabled={isRefreshing}>
+        <Text style={styles.loginText}>Login</Text>
+        {isRefreshing && (
+          <ActivityIndicator
+            size={'small'}
+            color={'white'}
+            animating={true}
+            style={{marginLeft: 8}}
+          />
+        )}
+      </TouchableOpacity>
+
+      <Text>Version : {currentVersion}</Text>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DCDCDC',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  inputContainer: {
+    borderBottomColor: '#F5FCFF',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 30,
+    borderBottomWidth: 1,
+    width: 300,
+    height: 45,
+    marginBottom: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+
+    shadowColor: '#808080',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  inputs: {
+    height: 45,
+    marginLeft: 16,
+    borderBottomColor: '#FFFFFF',
+    flex: 1,
   },
-  highlight: {
-    fontWeight: '700',
+  inputIcon: {
+    width: 30,
+    height: 30,
+    marginRight: 15,
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: 300,
+    borderRadius: 30,
+    backgroundColor: 'transparent',
+  },
+  btnForgotPassword: {
+    height: 15,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    marginBottom: 10,
+    width: 300,
+    backgroundColor: 'transparent',
+  },
+  loginButton: {
+    backgroundColor: '#00b5ec',
+
+    shadowColor: '#808080',
+    shadowOffset: {
+      width: 0,
+      height: 9,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 12.35,
+
+    elevation: 19,
+  },
+  loginText: {
+    color: 'white',
+  },
+  bgImage: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  },
+  btnText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
 
