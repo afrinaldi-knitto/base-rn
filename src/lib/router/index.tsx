c157960f7b85b770c1c51ef94886a520aca70a75
afrinaldi-knitto <it17.knitto@gmail.com>
2024-12-30 16:33:58 +0700

    ADD : flavor app

---
+++ src/lib/router/index.tsx
@@ -0,0 +1,4 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {RootStackParamList} from './stack';
+
+export const Stack = createNativeStackNavigator<RootStackParamList>();
---
+++ src/lib/router/stack.d.ts
@@ -0,0 +1,7 @@
+import {NavigationProp} from '@react-navigation/native';
+
+export type RootStackParamList = {
+  Login: undefined;
+};
+
+export type StackNavigation = NavigationProp<RootStackParamList>;
